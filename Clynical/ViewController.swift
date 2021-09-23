fd50f6ad98729707370a02c3982b5b592aec807c
Suraj <suraj.dayma11@gmail.com>
2021-09-23 14:10:52 +0400

    Initial Commit

---
+++ Clynical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clynical
+//
+//  Created by Suraj Dayma on 23/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
